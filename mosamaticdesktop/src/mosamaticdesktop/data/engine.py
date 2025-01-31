e3f977e4d4b59b954706c653f3ff189cfa7956f3
Ralph Brecheisen <r.brecheisen@maastrichtuniversity.nl>
2025-01-31 23:02:48 +0100

    Working on data manager

---
+++ mosamaticdesktop/src/mosamaticdesktop/data/engine.py
@@ -0,0 +1,19 @@
+import os
+
+from sqlalchemy import create_engine
+
+from mosamaticdesktop.singleton import singleton
+from mosamaticdesktop.data.models.basemodel import BaseModel
+
+DATABASE = 'db.sqlite3'
+DATABASEECHO = True if os.environ.get('DATABASEECHO', '0') =='1' else False
+
+
+@singleton
+class Engine:
+    def __init__(self):
+        self._engine = create_engine(f'sqlite:///{DATABASE}', echo=DATABASEECHO)
+        BaseModel.metadata.create_all(self._engine)
+
+    def get(self):
+        return self._engine
