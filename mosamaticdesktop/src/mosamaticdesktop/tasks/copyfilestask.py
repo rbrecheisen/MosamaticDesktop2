e468bb4beb4c4506863788bf00f1f6fc25a5da41
Brecheisen <r.brecheisen@maastrichtuniversity.nl>
2025-02-06 15:37:40 +0100

    Saving work

--- mosamaticdesktop/src/mosamaticdesktop/tasks/copyfilestask.py
+++ mosamaticdesktop/src/mosamaticdesktop/tasks/copyfilestask.py
@@ -9,6 +9,12 @@ class CopyFilesTask(Task):
     def __init__(self, input_dir, output_dir, params=None):
         super(CopyFilesTask, self).__init__(input_dir, output_dir, params)
 
+    def process_file(self, f):
+        source = os.path.join(self._input_dir, f)
+        target = os.path.join(self._output_dir, f)
+        shutil.copy(source, target)
+        print(f'Copied {source} to {target}')
+
     def execute(self):
         files = os.listdir(self._input_dir)
         nr_steps = len(files)
@@ -16,11 +22,9 @@ class CopyFilesTask(Task):
             if self._canceled:
                 self.set_status(TaskStatus.CANCELED)
                 return 
-            f = files[step]
-            source = os.path.join(self._input_dir, f)
-            target = os.path.join(self._output_dir, f)
-            shutil.copy(source, target)
-            print(f'Copied {source} to {target}')
+
+            self.process_file(files[step])
+
             self.progress.emit(int(((step + 1) / (nr_steps)) * 100))
             if self._params and 'delay' in self._params.keys():
                 time.sleep(self._params['delay'])
