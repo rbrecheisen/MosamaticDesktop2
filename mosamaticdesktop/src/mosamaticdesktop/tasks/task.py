840becda4346086ba6689f18cd3883cd558d5ac0
Ralph Brecheisen <r.brecheisen@maastrichtuniversity.nl>
2025-02-08 20:17:55 +0100

    Updated comments

--- mosamaticdesktop/src/mosamaticdesktop/tasks/task.py
+++ mosamaticdesktop/src/mosamaticdesktop/tasks/task.py
@@ -25,13 +25,13 @@ class Task(QThread):
     Parameters
     ----------
     input_dir : str
-        Input directory from which to read files
+        Input directory from which to read files.
     output_dir_name : str 
         Name of output directory where output results will be
         written. Output directory will be created at same level as input directory.
         If output directory already exists, a sequence number will be appended.
     params : dict
-        Task parameter dictionary
+        Task parameter dictionary.
 
     Returns
     -------
@@ -59,7 +59,7 @@ class Task(QThread):
     Returns
     -------
     str
-        Input directory
+        Input directory.
     """
     def get_input_dir(self):
         return self._input_dir
@@ -70,7 +70,7 @@ class Task(QThread):
     Returns
     -------
     str
-        Output directory
+        Output directory.
     """
     def get_output_dir(self):
         return self._output_dir
@@ -81,12 +81,12 @@ class Task(QThread):
     Parameters
     ----------
     name : str
-        Name of parameter
+        Name of parameter.
 
     Returns
     -------
     bool 
-        True or false
+        True or false.
     """
     def has_param(self, name):
         if self._params is not None:
@@ -94,18 +94,50 @@ class Task(QThread):
         return False
     
     """
+    Returns task's parameters.
+
+    Returns
+    -------
+    dict
+        Parameter dictionary.
     """
     def get_params(self):
         return self._params
     
+    """
+    Returns specific task parameter value or a default.
+
+    Parameters
+    ----------
+    name : str
+        Name of parameter.
+    default : Any
+        Default value for parameter value if parameter does not exist.
+
+    Returns
+    -------
+    Any
+        Parameter value (can be default).
+    """
     def get_param(self, name, default=None):
         if self.has_param(name):
             return self._params[name]
         return default
     
+    """
+    Checks if task has been canceled.
+
+    Returns
+    -------
+    bool
+        True or false.
+    """
     def is_canceled(self):
         return self._canceled
 
+    """
+    Executes task instance.
+    """
     def execute(self):
         for i in range(5):
             if self._canceled:
@@ -114,6 +146,10 @@ class Task(QThread):
             time.sleep(1)
             self.set_progress(i, 5)
 
+    """
+    Runs task instance. Can be called directly or through QThread's
+    start() method.
+    """
     def run(self):
         self.set_status(TaskStatus.RUNNING)
         try:
@@ -128,14 +164,37 @@ class Task(QThread):
             print(f'ERROR: Task failed ({e})')
             self.set_status(TaskStatus.FAILED)
 
+    """
+    Cancels task instance. Sets self._canceled boolean to True so
+    that the task picks it up on the next iteration and stops.
+    """
     def cancel(self):
         self._canceled = True
 
+    """
+    Sets progress of the task depending on the number of steps needed
+    and the current step.
+
+    Parameters
+    ----------
+    step : int
+        Index of the current iteration.
+    nr_steps : int
+        Total number of steps for this task.
+    """
     def set_progress(self, step, nr_steps):
         progress = int(((step + 1) / (nr_steps)) * 100)
         print(f'Progress task {self.__class__.__name__}: {progress}')
         self.progress.emit(progress)
 
+    """
+    Sets task status. 
+
+    Parameters
+    ----------
+    status : TaskStatus
+        Current status of task instance to be broadcast to application.
+    """
     def set_status(self, status):
         print(f'Status task {self.__class__.__name__}: {status.value}')
         self.status.emit(status.value)
