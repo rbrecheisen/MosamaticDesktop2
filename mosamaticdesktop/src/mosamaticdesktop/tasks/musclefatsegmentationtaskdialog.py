a8416ff6f790c19581cd4b9b3202d49106f49a4c
Ralph Brecheisen <r.brecheisen@maastrichtuniversity.nl>
2025-02-12 20:27:32 +0100

    Working on laying out Task dialog children

--- mosamaticdesktop/src/mosamaticdesktop/tasks/musclefatsegmentationtaskdialog.py
+++ mosamaticdesktop/src/mosamaticdesktop/tasks/musclefatsegmentationtaskdialog.py
@@ -1,42 +1,26 @@
-from PySide6.QtWidgets import QDialog, QPushButton, QFileDialog, QLabel, QVBoxLayout, QHBoxLayout
+from PySide6.QtWidgets import QPushButton, QFileDialog, QLabel, QVBoxLayout, QHBoxLayout
+
+from mosamaticdesktop.tasks.taskdialog import TaskDialog
 
 
-class MuscleFatSegmentationTaskDialog(QDialog):
+class MuscleFatSegmentationTaskDialog(TaskDialog):
     def __init__(self, parent=None):
         super(MuscleFatSegmentationTaskDialog, self).__init__(parent)
-        self.setWindowTitle('MuscleFatSegmentationTask parameters')
-        self._task_params = None
         self._model_dir = None
         self._open_model_dir_button = QPushButton('Open model directory', self)
         self._open_model_dir_button.clicked.connect(self.open_model_directory)
         self._model_dir_label = QLabel('Model directory:', self)
-        accept_button = QPushButton('Save', self)
-        accept_button.clicked.connect(self.accept)
-        cancel_button = QPushButton('Cancel', self)
-        cancel_button.clicked.connect(self.reject)
-        button_layout = QHBoxLayout()
-        button_layout.addWidget(accept_button)
-        button_layout.addWidget(cancel_button)
+
+    def get_layout(self):
         layout = QVBoxLayout()
         layout.addWidget(self._open_model_dir_button)
         layout.addWidget(self._model_dir_label)
-        layout.addLayout(button_layout)
-        self.setLayout(layout)
+        return layout
 
     def open_model_directory(self):
         self._model_dir = QFileDialog.getExistingDirectory(self, 'Select directory')
         if self._model_dir:
             self._model_dir_label.setText(f'Model directory: {self._model_dir}')
 
-    def accept(self):
-        if self._task_params is None:
-            self._task_params = {}
-        self._task_params['model_dir'] = self._model_dir
-        super(MuscleFatSegmentationTaskDialog, self).accept()
-
-    def reject(self):
-        self._task_params = None
-        super(MuscleFatSegmentationTaskDialog, self).reject()
-
-    def get_params(self):
-        return self._task_params
+    def update_params(self):
+        self.set_param('model_dir', self._model_dir)
--- mosamaticdesktop/src/mosamaticdesktop/tasks/taskdialog.py
+++ mosamaticdesktop/src/mosamaticdesktop/tasks/taskdialog.py
@@ -1,6 +1,43 @@
-from PySide6.QtWidgets import QDialog
+from PySide6.QtWidgets import QDialog, QVBoxLayout, QHBoxLayout, QPushButton
 
 
 class TaskDialog(QDialog):
     def __init__(self, parent=None):
         super(TaskDialog, self).__init__(parent)
+        self.setWindowTitle(self.__class__.__name__)
+        self._params = None
+        self._save_button = QPushButton('Save', self)
+        self._save_button.clicked.connect(self.accept)
+        self._cancel_button = QPushButton('Cancel', self)
+        self._cancel_button.clicked.connect(self.reject)
+        button_layout = QHBoxLayout()
+        button_layout.addWidget(self._save_button)
+        button_layout.addWidget(self._cancel_button)
+        layout = QVBoxLayout()
+        child_layout = self.get_layout()
+        if child_layout:
+            layout.addLayout(self.get_layout())
+        layout.addLayout(button_layout)
+        self.setLayout(layout)
+
+    def get_layout(self):
+        raise NotImplementedError('Implement in child dialog')
+    
+    def update_params(self):
+        raise NotImplementedError('Implement in child dialog')
+    
+    def set_param(self, name, value):
+        if self._params is None:
+            self._params = {}
+        self._params[name] = value
+    
+    def accept(self):
+        self._params = self.update_params()
+        super(CopyFilesTaskDialog, self).accept()
+
+    def reject(self):
+        self._params = None
+        super(CopyFilesTaskDialog, self).reject()
+
+    def get_params(self):
+        return self._params
