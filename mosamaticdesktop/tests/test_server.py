76477d25019713c9bd25796ae8ee29bb5ebf1810
Ralph Brecheisen <r.brecheisen@maastrichtuniversity.nl>
2025-02-12 17:56:59 +0100

    Trying to get server tests working on Mac

--- mosamaticdesktop/tests/test_server.py
+++ mosamaticdesktop/tests/test_server.py
@@ -1,9 +1,20 @@
+import sys
 import requests
 
 
 def test_server():
+    input_dir = 'D:\\Mosamatic\\Mosamatic Desktop 2.0\\input' \
+        if sys.platform.startswith('win') \
+        else '/Users/ralph/Desktop/downloads/mosamaticdesktop/input'
+    config_file_path = 'D:\\SoftwareDevelopment\\GitHub\\MosamaticDesktop2\\pipeline_windows.yaml' \
+        if sys.platform.startswith('win') \
+        else '/Users/ralph/dev/tools/MosamaticDesktop2/pipeline_unix.yaml'
+
+    print(input_dir)
+    print(config_file_path)
+    
     response = requests.post('http://localhost:5000/pipeline', json={
-        'input_dir': 'D:\\Mosamatic\\Mosamatic Desktop 2.0\\input',
-        'config_file_path': 'D:\\SoftwareDevelopment\\GitHub\\MosamaticDesktop2\\pipeline_windows.yaml',
+        'input_dir': input_dir,
+        'config_file_path': config_file_path,
     })
     assert response.status_code == 200, response.status_code
