c8e393adfe6eabf566e06356ed4207b474351808
Brecheisen <r.brecheisen@maastrichtuniversity.nl>
2025-02-13 16:30:16 +0100

    Testing calculate metrics task

--- README.md
+++ README.md
@@ -5,9 +5,12 @@ Desktop and server version of Mosamatic
 - briefcase create (if new packages added to .toml)
 
 ## Building executables
+- briefcase dev
+- briefcase dev -- --server (start in server mode)
 - briefcase update
 - briefcase build --app <app_name>
-- briefcase run --app <app_name>
+- briefcase run
+- briefcase run -- --server
 - briefcase package --app <app_name>
 
 
--- mosamaticdesktop/src/mosamaticdesktop/tasks/calculatemetricstask.py
+++ mosamaticdesktop/src/mosamaticdesktop/tasks/calculatemetricstask.py
@@ -8,20 +8,25 @@ class CalculateMetricsTask(Task):
     def __init__(self, input_dir, output_dir_name=None, params=None):
         super(CalculateMetricsTask, self).__init__(input_dir, output_dir_name, params)
 
+    def collect_img_seg_pairs(img_files, seg_files):
+        for img_f in img_files:
+            print(img_f)
+
     def execute(self):
         seg_files = os.listdir(self.get_input_dir())
         img_files = os.listdir(self.get_param('image_dir', None))
-        nr_steps = len(seg_files)
+        img_seg_pairs = self.collect_img_seg_pairs(img_files, seg_files)
+
+        nr_steps = len(img_seg_pairs)
         for step in range(nr_steps):
             if self.is_canceled():
                 self.set_status(TaskStatus.CANCELED)
                 return 
 
-            # Copy source to target
-            f = seg_files[step]
-            source = os.path.join(self.get_input_dir(), f)
-            target = os.path.join(self.get_output_dir(), f)
-            shutil.copy(source, target)
+            # f = seg_files[step]
+            # source = os.path.join(self.get_input_dir(), f)
+            # target = os.path.join(self.get_output_dir(), f)
+            # shutil.copy(source, target)
 
             # Update progress
             self.set_progress(step, nr_steps)
